9731206a91b70e65b287d59c7cd92fec9ccfd271
duanxin <changfu.scf@antgroup.com>
2025-11-27 22:40:03 +0800

    fix

--- cphtml.sh
+++ cphtml.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+rm -rf /usr/share/nginx/html.bak
 mv /usr/share/nginx/html /usr/share/nginx/html.bak
 cp -r ./static /usr/share/nginx/html
 find /usr/share/nginx/html -type d -exec chmod 755 {} \;
